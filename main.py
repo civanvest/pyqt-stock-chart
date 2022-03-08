71b0d987b100041ebc60005c2387b29a1e147319
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-03-08 22:36:48 +0900

    feat: add search form inside sidebar

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('Streamlit Application')
+
+st.sidebar.title('Sidebar Title')
+form = st.sidebar.form(key='my-form')
+symbol = form.text_input('Symbol')
+symbol = symbol.upper()
+search = form.form_submit_button('Search')
+if search:
+    st.write(symbol)
