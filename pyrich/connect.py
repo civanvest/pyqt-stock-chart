c7e3a09ba850a466873ac82a8d80989c20592063
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-03-09 22:08:58 +0900

    refactor: use postgresql instead of mysql

--- pyrich/connect.py
+++ pyrich/connect.py
@@ -1,72 +1,30 @@
+import pandas as pd
 import sqlalchemy
-from sqlalchemy import create_engine, inspect
-import pymysql
-
+from sqlalchemy.engine.base import Engine
+import streamlit as st
 
 class Database:
-    def __init__(self, rdbms: str, dbapi: str, user: str, pw: str,
-                 host: str, db_name: str) -> None:
-        """Connect to a database.
 
-        :param rdbms: Relational database management system.
-        :type rdbms: str
-        :param dbapi: Database api.
-        :type dbapi: str
-        :param user: RDBMS username.
-        :type user: str
-        :param pw: RDBMS password.
-        :type pw: str
-        :param host: RDBMS host.
-        :type host: str
-        :param db_name: Database to work in.
-        :type db_name: str
-        """
+    def __init__(self, config: dict):
+        self.config = config
+        self.engine = self.connect()
 
-        self.rdbms = rdbms
-        self.dbapi = dbapi
-        self.user = user
-        self.pw = pw
-        self.host = host
-        self.db_name = db_name
-        self.engine, self.insp, self.connection, self.cursor = self.connect()
+    @st.cache(allow_output_mutation=True)
+    def connect(self):
+        user = self.config['user']
+        password = self.config['password']
+        host = self.config['host']
+        port = self.config['port']
+        dbname = self.config['dbname']
+        engine = sqlalchemy.create_engine(
+            f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
+            )
+        return engine
 
-    def connect(self) -> tuple[sqlalchemy.engine.Engine,
-                               sqlalchemy.engine.reflection.Inspector,
-                               pymysql.Connection,
-                               pymysql.cursors.Cursor]:
-        db_url = (f'{self.rdbms}+{self.dbapi}://'
-                  f'{self.user}:{self.pw}@{self.host}/')
-        engine = create_engine(
-            url=db_url,
-            encoding='utf-8',
-        )
-        connection = pymysql.connect(
-            host=self.host,
-            user=self.user,
-            password=self.pw,
-        )
+    @st.cache(hash_funcs={Engine: id})
+    def run(self, query: str):
+        result = pd.read_sql(query, self.engine)
+        return result
 
-        insp = inspect(engine)
-        cursor = connection.cursor()
-
-        try:
-            self.setup_db()
-        except (pymysql.ProgrammingError, pymysql.OperationalError) as e:
-            print(e)
-        finally:
-            return engine, insp, connection, cursor
-        
-
-    def setup_db(self) -> None:
-        """Choose a database to operate."""
-        query = f'CREATE DATABASE IF NOT EXISTS {self.db_name}'
-        self.cursor.execute(query)
-
-        query = f'USE {self.db_name}'
-        self.cursor.execute(query)
-
-        self.connection.commit()
-
-    def __del__(self) -> None:
-        self.cursor.close()
-        self.connection.close()
+    def __del__(self):
+        self.engine.dispose()
