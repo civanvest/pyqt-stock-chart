fe6934e3aac364685e485f99d91befabace6e1cc
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-03-09 22:09:21 +0900

    feat: add database connection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from pyrich.connect import Database
 
+
+CONFIG = st.secrets['postgres']
 st.title('Streamlit Application')
+
+db = Database(CONFIG)
 
 st.sidebar.title('Sidebar Title')
 form = st.sidebar.form(key='my-form')
