d55cd471de19b0ee7055a280c0500a10e153181d
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-03-11 16:54:07 +0900

    refactor: set new interface

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import pandas as pd
 import streamlit as st
 from pyrich.connect import Database
 
 
 CONFIG = st.secrets['postgres']
-st.title('Streamlit Application')
+st.title('Portfolio')
 
 db = Database(CONFIG)
 
-st.sidebar.title('Sidebar Title')
-form = st.sidebar.form(key='my-form')
-symbol = form.text_input('Symbol')
-symbol = symbol.upper()
-search = form.form_submit_button('Search')
-if search:
-    result = db.run('SELECT * FROM orders')
-    st.dataframe(result)
+st.header('Summary')
+st.write('show summary here')
+# result = db.run('SELECT * FROM orders')
+# st.dataframe(result)
+
+st.header('Details')
+st.write('show details here')
+
+st.header('Visualization')
+st.write('show visualization here')
