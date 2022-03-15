0ae0e4ecd5c061119c4d12b5c7846bb5fa1cb294
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-03-15 21:37:03 +0900

    feat: add getting date of n days ago

--- pyqt/date.py
+++ pyqt/date.py
@@ -2,6 +2,11 @@ from datetime import datetime
 import pandas as pd
 
 
+def get_n_days_ago(days):
+    today = pd.Timestamp.today().normalize()
+    date = subtract_days(today, days)
+    return date
+
 def subtract_days(date, days):
     n_days = pd.Timedelta(days=days)
     date = date - n_days
