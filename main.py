44a946f1410dc5f6eec7237ca9c1c84c712816c2
choi-jiwoo <cho2.jiwoo@gmail.com>
2022-03-17 12:11:13 +0900

    feat: add reading watchlist.csv

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import pandas as pd
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 
+
 class Window(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.read_data()
         self.init_ui()
+
+    def read_data(self):
+        self.data_path = resource_path('data/watchlist.csv')
+        self.data = pd.read_csv(self.data_path, dtype=str)
+        self.data[['Name', 'Crypto']] = self.data[['Name', 'Crypto']].fillna('')
+
     def init_ui(self):
         self.setWindowTitle('Stock Chart') 
         self.resize(800, 1000)
